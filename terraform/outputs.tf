50d6faee82b45a009f9f09180839ca794b68a846
Roy De Jesus <roydejesus1031@gmail.com>
2022-06-26 21:16:38 -0500

    Move comments to descriptions in tf files
    - Add some comments to vars in tf files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,23 +1,23 @@
-# The provider ID that has to be given to Github Actions to deploy the website
 output "site_deployer_provider_id" {
   value     = "projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/${var.gh_site_deploy_pool_id}/providers/${var.gh_site_deploy_prov_id}"
   sensitive = true
+  description = "The provider ID that has to be given to Github Actions to deploy the website"
 }
 
-# The Service Account that has to be given to Github Actions to deploy the website
 output "site_deployer_svc_acc" {
   value     = google_service_account.site_deployer.email
   sensitive = true
+  description = "The Service Account that has to be given to Github Actions to deploy the website"
 }
 
-# The provider ID that has to be given to Github Actions to manage Terraform
 output "tf_deployer_provider_id" {
   value     = "projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/${var.gh_tf_deploy_pool_id}/providers/${var.gh_tf_deploy_prov_id}"
   sensitive = true
+  description = "The provider ID that has to be given to Github Actions to manage Terraform"
 }
 
-# The Service Account that has to be given to Github Actions to manage Terraform
 output "tf_deployer_svc_acc" {
   value     = google_service_account.tf_deployer.email
   sensitive = true
+  description = "The Service Account that has to be given to Github Actions to manage Terraform"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,56 +1,67 @@
 variable "project_id" {
   type    = string
   default = "website-353206"
+  description = "GCP Project ID"
 }
 
 variable "github_repo" {
   type    = string
   default = "roydejesus1031/website"
+  description = "Github repo to allow the workload identity for"
 }
 
 variable "state_bucket_name" {
   type    = string
   default = "roy-dejesus-website-prod-tfstate"
+  description = "Remote GCP storage bucket for state"
 }
 
 variable "website_url" {
   type    = string
   default = "roydejesus.com"
+  description = "URL for website"
 }
 
 variable "email" {
   type    = string
   default = "roydejesus1031@gmail.com"
+  description = "Email to notify for alerts"
 }
 
 variable "region" {
   type    = string
   default = "us-central1"
+  description = "GCP region"
 }
 
 variable "zone" {
   type    = string
   default = "us-central1-c"
+  description = "GCP zone"
 }
 
 variable "gh_site_deploy_pool_id" {
   type    = string
   default = "github-site-identity-pool"
+  description = "ID for identity pool for site deployment used by Github"
 }
 
 variable "gh_site_deploy_prov_id" {
   type    = string
   default = "github-site-identity-provider"
+  description = "ID for identity provider for site deployment used by Github"
 }
 
 variable "gh_tf_deploy_pool_id" {
   type    = string
   default = "github-tf-identity-pool"
+  description = "ID for identity pool for Terraform used by Github"
 }
 
 variable "gh_tf_deploy_prov_id" {
   type    = string
   default = "github-tf-identity-provider"
+  description = "ID for identity provider for Terraform used by Github"
 }
 
 variable "services" {
@@ -63,4 +74,5 @@ variable "services" {
     "compute.googleapis.com",
     "iamcredentials.googleapis.com"
   ]
+  description = "GCP services that need to be enabled"
 }
